7a9f3b79b2af58365eff1f7785d74dca8c4b3863
Tal Atlas <me@tal.by>
2011-09-27 14:23:50 -0400

    bugfixes

--- lib/freyr/command.rb
+++ lib/freyr/command.rb
@@ -3,14 +3,17 @@ module Freyr
   class Command
     extend Forwardable
     
-    attr_reader :command, :name, :service
+    attr_reader :name, :service
     
     def initialize(service, command=nil, args = {})
       @service = service
-      @info = service.info
-      @name = @info.name
-      @command = @info.start
-      @env = @info.env
+    end
+
+    def_delegators :'@service', :info
+    def_delegators :info, :name, :env
+
+    def command
+      info.start
     end
     
     def run!
@@ -24,14 +27,15 @@ module Freyr
       pid = spawn(command)
       
       Freyr.logger.debug("attempting to run command") {command.inspect}
-      
+      str = "\nStarting #{info.name} with #{command.inspect}"
+      OUT.puts '',"Starting #{info.name} with #{command.inspect}", '='*str.length
       Process.detach(pid)
       
       pid = service.pid_file.wait_for_pid
       
-      OUT.puts "PID of new #{@info.name} process is #{pid}"
+      OUT.puts "PID of new #{info.name} process is #{pid}"
       
-      if @info.ping
+      if info.ping
         pinger = Pinger.new(@service)
         
         pinger.wait_for_resp { @service.alive? }
@@ -41,13 +45,13 @@ module Freyr
         OUT.puts "Launch took about #{(Time.now-total_time).ceil} seconds"
         pid
       else
-        OUT.puts "#{@info.name} service wasn't launched correctly. For details see: #{@info.log}"
+        OUT.puts "#{info.name} service wasn't launched correctly. For details see: #{info.log}"
       end
     end
     
     def kill!(sig=nil)
       require_admin
-      sig ||= @info.stop_sig || 'KILL'
+      sig ||= info.stop_sig || 'KILL'
       
       Freyr.logger.debug("sending signal to process") {"Signal: #{sig}, PID: #{pid}"}
       
@@ -63,11 +67,11 @@ module Freyr
     def restart!
       require_admin
       
-      if @info.restart
+      if info.restart
         chdir
-        system(@info.restart)
-      elsif @info.restart_sig
-        kill!(@info.restart_sig)
+        system(info.restart)
+      elsif info.restart_sig
+        kill!(info.restart_sig)
       else
         run!
       end
@@ -76,50 +80,50 @@ module Freyr
     private
     
     def require_admin
-      raise AdminRequired if @info.sudo && !Freyr.is_root?
+      raise AdminRequired if info.sudo && !Freyr.is_root?
     end
     
     def chdir
-      Dir.chdir File.expand_path(@info.dir||'/')
+      Dir.chdir File.expand_path(info.dir||'/')
     end
     
     def spawn(command)
-      if @info.rvm && RVM.installed?
-        Freyr.logger.debug('attempting to set rvm') {@info.rvm}
-        if RVM.installed?(@info.rvm)
-          command = "rvm #{@info.rvm} exec #{command}"
+      if info.rvm && RVM.installed?
+        Freyr.logger.debug('attempting to set rvm') {info.rvm}
+        if RVM.installed?(info.rvm)
+          command = "rvm #{info.rvm} exec #{command}"
           Freyr.logger.debug('changed command to') {command}
         else
-          abort("must setup rvm correctly, run: rvm --install --create #{@info.rvm}")
+          abort("must setup rvm correctly, run: rvm --install --create #{info.rvm}")
         end
-      elsif @info.rvm
-        Freyr.logger.debug("rvm not installed so can't switch to") {@info.rvm}
+      elsif info.rvm
+        Freyr.logger.debug("rvm not installed so can't switch to") {info.rvm}
       end
 
       fork do
-        File.umask @info.umask if @info.umask
-        uid_num = Etc.getpwnam(@info.uid).uid if @info.uid
-        gid_num = Etc.getgrnam(@info.gid).gid if @info.gid
+        File.umask info.umask if info.umask
+        uid_num = Etc.getpwnam(info.uid).uid if info.uid
+        gid_num = Etc.getgrnam(info.gid).gid if info.gid
         
-        ::Dir.chroot(@info.chroot) if @info.chroot
+        ::Dir.chroot(info.chroot) if info.chroot
         ::Process.setsid
-        ::Process.groups = [@info.guid] if @info.gid
-        ::Process::Sys.setgid(@info.guid) if @info.gid
-        ::Process::Sys.setuid(@info.guid) if @info.uid
+        ::Process.groups = [info.guid] if info.gid
+        ::Process::Sys.setgid(info.guid) if info.gid
+        ::Process::Sys.setuid(info.guid) if info.uid
         chdir
         $0 = "freyr - #{name} (#{command})"
         STDIN.reopen "/dev/null"
-        if @info.log_cmd
+        if info.log_cmd
           STDOUT.reopen IO.popen(log_cmd, "a")
-        elsif @info.log && !@info.dont_write_log
-          STDOUT.reopen @info.log, "a"
+        elsif info.log && !info.dont_write_log
+          STDOUT.reopen info.log, "a"
         else
           STDOUT.reopen "/dev/null"
         end
-        if @info.err_log_cmd
-          STDERR.reopen IO.popen(@info.err_log_cmd, "a") 
-        elsif @info.err_log && (@info.log_cmd || @info.err_log != @info.log)
-          STDERR.reopen @info.err_log, "a"
+        if info.err_log_cmd
+          STDERR.reopen IO.popen(info.err_log_cmd, "a") 
+        elsif info.err_log && (info.log_cmd || info.err_log != info.log)
+          STDERR.reopen info.err_log, "a"
         else
           STDERR.reopen STDOUT
         end
